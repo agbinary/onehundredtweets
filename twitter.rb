c09151d9e9a9754ad7078eb145c2516911e5b905
angela4713 <ang3l_gu@hotmail.com>
2014-03-15 00:05:10 -0500

    onehundredtwitter

--- twitter.rb
+++ twitter.rb
@@ -20,35 +20,40 @@ twitter_db = Mysql2::Client.new(host: "localhost", username: "root", password: "
 puts "De que usuario quiere guardar los ultimos 100 tweets? "
 user=gets.chomp
 
-client.user_timeline(user, :count => 100).each do |tweet|
-  h="INSERT INTO tweets.tweet (user, tweet) VALUES ( '#{user}', '#{tweet.text.gsub(/'/, " ")}')";
-  twitter_db.query h
+begin
+  client.user_timeline(user)
+rescue Twitter::Error::NotFound
+  puts "El usuario no existe";
+else
+  client.user_timeline(user, :count => 100).each do |tweet|
+    h="INSERT INTO tweets.tweet (user, tweet) VALUES ( '#{user}', '#{tweet.text.gsub(/'/, " ")}')";
+    twitter_db.query h
+  end
+
+  result=twitter_db.query "SELECT * FROM tweet"
+  todos=""
+  result.each {|x| todos << x["tweet"] + "<br>"}
+
+  puts "\nA que e-mail desea enviar la base de datos? "
+  email=gets.chomp
+
+  url = "https://sendgrid.com/api/mail.send.json"
+
+  response = HTTParty.post url, :body => {
+      "api_user" => ENV["SENDGRID_USER"],
+      "api_key" => ENV["SENDGRID_TOKEN"],
+      "to" => email,
+      "from" => "ang3l_gu@hotmail.com",
+      "subject" => "OneHundredTweets",
+      "html" => "Estos son los ultimos 100 tweets del usuario #{user}: <p><p> " + todos
+
+  }
+  response.body
+  puts "El e-mail fue enviado"
+
 end
 
-if client.search(user)==NoMethodError
-  puts "El usuario no existe"
-end
 
-result=twitter_db.query "SELECT * FROM tweet"
-todos=""
-result.each {|x| todos << x["tweet"] + "<br>"}
-
-puts "\nA que e-mail desea enviar la base de datos? "
-email=gets.chomp
-
-url = "https://sendgrid.com/api/mail.send.json"
-
-response = HTTParty.post url, :body => {
-    "api_user" => "ang3l_gu",
-    "api_key" => "57-33-00.a",
-    "to" => email,
-    "from" => "ang3l_gu@hotmail.com",
-    "subject" => "OneHundredTweets",
-    "html" => "Estos son los ultimos 100 tweets del usuario #{user}: <p><p> " + todos
-
-}
-response.body
-puts "El e-mail fue enviado"
 
 
 
