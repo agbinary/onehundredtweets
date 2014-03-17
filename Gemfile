dff4fda2cfa142d1b270fb21ee5327afff24d6ba
angela4713 <ang3l_gu@hotmail.com>
2014-03-17 13:19:26 -0500

    gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+gem 'oauth'
+gem 'twitter'
+gem 'json'
+gem 'mysql2'
+gem 'httparty'
+gem "minitest", :group => :test
+gem "mocha", :group => :test
--- twitter.rb
+++ twitter.rb
@@ -24,22 +24,23 @@ class OneHundredTwitter
     @user = user
     @db = db
     @email = email
-    @message = ""
   end
 
   def add_db
     @client.user_timeline(@user, :count => 100).each do |tweet|
-    h = "INSERT INTO tweets.tweet (user, tweet) VALUES ( '#{@user}', '#{tweet.text.gsub(/'/, " ")}')";
-    @db.query h
+      query = "INSERT INTO tweets.tweet (user, tweet) VALUES ( '#{@user}', '#{tweet.text.gsub(/'/, " ")}')";
+      @db.query(query)
     end
   end
 
   def create_message
+    message = ""
     result = @db.query "SELECT * FROM tweet"
-    result.each {|x| @message << x["tweet"] + "<br>"}
+    result.each {|x| message << x["tweet"] + "<br>"}
+    message
   end
 
-  def send_email
+  def send_email(message)
     url = "https://sendgrid.com/api/mail.send.json"
     response = HTTParty.post url, :body => {
         "api_user" => ENV["SENDGRID_USER"],
@@ -47,7 +48,7 @@ class OneHundredTwitter
         "to" => @email,
         "from" => "ang3l_gu@hotmail.com",
         "subject" => "OneHundredTweets",
-        "html" => "Estos son los ultimos 100 tweets del usuario #{@user}: <p><p> " + @message
+        "html" => "Estos son los ultimos 100 tweets del usuario #{@user}: <p><p> " + message
     }
 
     response.body
@@ -67,11 +68,11 @@ else
   email = gets.chomp
   p = OneHundredTwitter.new(client, user, twitter_db, email)
   p.add_db
-  p.create_message
-  p.send_email
+  message = p.create_message
+  p.send_email(message)
 end
 
- 
+
 
 
 
