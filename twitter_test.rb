52dc6c47fa17e46dc36dd8fc6ed8b87edd9312c7
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-18 01:18:55 -0500

    Add test for send_email

--- twitter_test.rb
+++ twitter_test.rb
@@ -16,6 +16,7 @@ class OneHunderdTwitterTest < Minitest::Test
 
     assert app.add_db
   end
+
   def test_create_message
     client = mock
     db = mock
@@ -25,5 +26,43 @@ class OneHunderdTwitterTest < Minitest::Test
     app = OneHundredTwitter.new(client, 'guille', db, 'angie@gmail.com')
 
     assert_equal "Hello, Angie<br>", app.create_message
+  end
+
+  def test_send_email
+    sendgrid_user = ENV["SENDGRID_USER"]
+    sendgrid_token = ENV["SENDGRID_TOKEN"]
+
+    ENV["SENDGRID_USER"] = "user"
+    ENV["SENDGRID_TOKEN"] = "key"
+
+    hash = {
+      :body => {
+        "api_user" => "user",
+        "api_key" => "key",
+        "to" => "angie@gmail.com",
+        "from" => "ang3l_gu@hotmail.com",
+        "subject" => "OneHundredTweets",
+        "html" => "Estos son los ultimos 100 tweets del usuario guille: <p><p> Hola, Angie<br>"
+      }
+    }
+
+    client = mock
+    db = mock
+    response = mock
+
+    response.expects(:body)
+
+    HTTParty.expects(:post).with("https://sendgrid.com/api/mail.send.json", hash).returns(response)
+
+    app = OneHundredTwitter.new(client, 'guille', db, 'angie@gmail.com')
+
+    out, _ = capture_io do
+      app.send_email("Hola, Angie<br>")
+    end
+
+    assert_equal "El e-mail fue enviado\n", out
+  ensure
+    ENV["SENDGRID_USER"] = sendgrid_user
+    ENV["SENDGRID_TOKEN"] = sendgrid_token
   end
 end
