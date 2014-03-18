97ce315dd27d4fc259c9bd42a9f27594d47ce59a
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-18 01:29:41 -0500

    DRY: clean tests

--- twitter_test.rb
+++ twitter_test.rb
@@ -3,27 +3,27 @@ require "minitest/autorun"
 require "mocha/mini_test"
 
 class OneHunderdTwitterTest < Minitest::Test
+  def setup
+    @client = mock
+    @db = mock
+  end
+
   def test_add_db
-    client = mock
     tweet = mock
-    db = mock
 
     tweet.expects(:text).returns("Hello, Angie")
-    client.expects(:user_timeline).returns([tweet])
-    db.expects(:query).with("INSERT INTO tweets.tweet (user, tweet) VALUES ( 'guille', 'Hello, Angie')").returns(true)
+    @client.expects(:user_timeline).returns([tweet])
+    @db.expects(:query).with("INSERT INTO tweets.tweet (user, tweet) VALUES ( 'guille', 'Hello, Angie')").returns(true)
 
-    app = OneHundredTwitter.new(client, 'guille', db, 'angie@gmail.com')
+    app = OneHundredTwitter.new(@client, 'guille', @db, 'angie@gmail.com')
 
     assert app.add_db
   end
 
   def test_create_message
-    client = mock
-    db = mock
+    @db.expects(:query).with("SELECT * FROM tweet").returns([{"tweet" => "Hello, Angie"}])
 
-    db.expects(:query).with("SELECT * FROM tweet").returns([{"tweet" => "Hello, Angie"}])
-
-    app = OneHundredTwitter.new(client, 'guille', db, 'angie@gmail.com')
+    app = OneHundredTwitter.new(@client, 'guille', @db, 'angie@gmail.com')
 
     assert_equal "Hello, Angie<br>", app.create_message
   end
@@ -46,15 +46,13 @@ class OneHunderdTwitterTest < Minitest::Test
       }
     }
 
-    client = mock
-    db = mock
     response = mock
 
     response.expects(:body)
 
     HTTParty.expects(:post).with("https://sendgrid.com/api/mail.send.json", hash).returns(response)
 
-    app = OneHundredTwitter.new(client, 'guille', db, 'angie@gmail.com')
+    app = OneHundredTwitter.new(@client, 'guille', @db, 'angie@gmail.com')
 
     out, _ = capture_io do
       app.send_email("Hola, Angie<br>")
